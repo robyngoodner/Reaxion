1b2e9f78f60e452c88970da6580d4587cd04682e
robyngoodner <robyn.goodner@gmail.com>
2022-03-12 16:29:57 -0500

    Add restful routing templates to controllers

---
+++ server/controllers/users.js
@@ -0,0 +1,10 @@
+// Rest Routes
+/*
+ * Index - GET - /users  - Presentational - respond with all users
+ * New - GET - /users/new  - Presentational Form - a page with a form to create a new user
+ * Show - GET - /users/:id  - Presentational - respond with specific user by id
+ * Create - Post - /users  - Functional - recieve data from new route to create a user
+ * Edit - GET - /users/:id/edit  - Presentational Form - respond with a form prefilled with user data
+ * Update - PUT - /users/:id  - Functional - recieve data from edit to update a specific user
+ * Delete - DELETE - /users/:id  - Functional - Deletes user by id from request
+ */
