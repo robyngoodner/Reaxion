5a61a035c59a52a9d4d688f227a41f368ca01da5
ampersand-paris <ampersand.s.paris@gmail.com>
2022-03-12 17:36:03 -0500

    create controller

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -8,3 +8,24 @@
  * Update - PUT - /posts/:id  - Functional - recieve data from edit to update a specific post
  * Delete - DELETE - /posts/:id  - Functional - Deletes post by id from request
  */
+
+const db = require("../models");
+
+const create = (req, res) => {
+    db.Post. create(req.body, (err, savedPost) => {
+        if (err) {
+            return res.status(400).json({
+                message: "Error 400",
+                error: err 
+            })
+        }
+        return res.status(201).json({
+            message: "Created Post",
+            data: savedPost
+        })
+    })
+}
+
+module.exports = {
+    create,
+}    
