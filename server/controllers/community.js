88e6fa915c69b1c6ce0c7640283e657b2495f8c9
robyngoodner <robyn.goodner@gmail.com>
2022-03-12 16:47:43 -0500

    Create user model

--- server/controllers/community.js
+++ server/controllers/community.js
@@ -1,10 +1,10 @@
 // Rest Routes
 /*
- * Index - GET - /community  - Presentational - respond with all community
- * New - GET - /community/new  - Presentational Form - a page with a form to create a new community
- * Show - GET - /community/:id  - Presentational - respond with specific community by id
- * Create - Post - /community  - Functional - recieve data from new route to create a community
- * Edit - GET - /community/:id/edit  - Presentational Form - respond with a form prefilled with community data
- * Update - PUT - /community/:id  - Functional - recieve data from edit to update a specific community
- * Delete - DELETE - /community/:id  - Functional - Deletes community by id from request
+ * Index - GET - /communities  - Presentational - respond with all community
+ * New - GET - /communities/new  - Presentational Form - a page with a form to create a new community
+ * Show - GET - /communities/:id  - Presentational - respond with specific community by id
+ * Create - Post - /communities  - Functional - recieve data from new route to create a community
+ * Edit - GET - /communities/:id/edit  - Presentational Form - respond with a form prefilled with community data
+ * Update - PUT - /communities/:id  - Functional - recieve data from edit to update a specific community
+ * Delete - DELETE - /communities/:id  - Functional - Deletes community by id from request
  */
---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    email: String,
+    Communities: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Community"
+    }],
+    facilitator: {
+        type: Boolean
+    },
+    Facilitator_Communities: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Community"
+    }],
+    Posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }],
+    Comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+})
