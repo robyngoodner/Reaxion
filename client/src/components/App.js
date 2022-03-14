2c49cada86f68c32521742a5373927e6fd98c1ed
Karina <karinadgarces@gmail.com>
2022-03-14 09:37:23 -0400

    register component added to main

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import CommunityCreate from "./Community/CommunityCreate";
-
+import Register from "./Register";
 function App() {
 
   return (
     <div className="App">
       React App
       <CommunityCreate />
+      <Register />
     </div>
   );
 }
