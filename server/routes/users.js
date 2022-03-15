fbeb1143cd190a68e808b8982629f4b6df005a50
JasonxWarren <warrenjason1@gmail.com>
2022-03-15 18:02:54 -0400

    routing updated

---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+const {users} = require("../controllers")
+const authRequired = require("../middleware/auth.required")
+
+//** missing user controller logic */
+
+router.get("/", users.index)
+router.get("/profile", authRequired, users.show)
+router.get("profile/:id/edit", users.editProfile)
+router.post("/profile/update", users.updateProfile) 
+router.delete("/:id", users.destroy)
+
+module.exports = router;
