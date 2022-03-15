9e290530fe0793b00cf523784fe016f70f62c28a
Mohammad Turaab <mohammadturaab@gmail.com>
2022-03-14 20:17:59 -0500

    added show posts in event controller

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,4 +1,5 @@
 // Rest Routes
+const db = require('../models');
 /*
  * Index - GET - /events  - Presentational - respond with all event
  * New - GET - /events/new  - Presentational Form - a page with a form to create a new event
@@ -8,3 +9,28 @@
  * Update - PUT - /events/:id  - Functional - recieve data from edit to update a specific event
  * Delete - DELETE - /events/:id  - Functional - Deletes event by id from request
  */
+
+const show = (req, res) => {
+    db.Post.findById(req.params.id, (err, foundPost) => {
+        if (err) {
+            return res
+                .status(400)
+                .json({
+                    message: "Bad Request; Cannot view event",
+                    err: err,
+                })
+        }
+        return res
+            .status(200)
+            .json({
+                message: "Event Found",
+                data: foundPost
+            })
+    })
+}
+
+
+module.exports = {
+    show,
+
+}
