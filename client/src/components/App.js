c62e5ff474d776df0d96707cfca71c6d1b31f93d
robyngoodner <robyn.goodner@gmail.com>
2022-03-15 22:01:44 -0400

    Add react routes and pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,18 +2,24 @@ import UpdateUserProfile from "./UserProfile/UserProfileUpdate";
 import CommunityCreate from "./Community/CommunityCreate";
 import Register from "./Register";
 import CommunityJoin from "./Community/CommunityJoin";
-import PostCreate from "./Posts/PostCreate"
+import PostCreate from "./Posts/PostCreate";
+import { Link } from "react-router-dom";
+import NavBar from './NavBar';
+import Home from "../pages/HomePage";
 
 function App() {
 
   return (
     <div className="App">
-     < UpdateUserProfile/>
+      <NavBar />
+      
+      {/* < UpdateUserProfile/> */}
       <h1>Reaxion</h1>
-      <CommunityCreate />
+      <Home />
+      {/* <CommunityCreate />
       <Register />
       <CommunityJoin />
-      <PostCreate />
+      <PostCreate /> */}
     </div>
   );
 }
