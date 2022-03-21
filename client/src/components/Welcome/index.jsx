3180308f9c2c4328e1b1aeaa8ce5053a234e27e9
Karina <karinadgarces@gmail.com>
2022-03-21 11:52:30 -0400

    added reactions on loginpage

--- client/src/components/Welcome/index.jsx
+++ client/src/components/Welcome/index.jsx
@@ -8,7 +8,19 @@ const Welcome = ({ checkUserActive }) => {
 	return (
 		<div>
 			<div className="center">
-				<h1 className="title">Reaxion</h1>
+			<div>
+			<h1 className="title">Reaxion</h1>
+				<div class="reactionimgs">
+				<img className="resizeReactions" src="../images/angry.png" alt="angry"></img>
+				<img className="resizeReactions" src="../images/content.png" alt="content"></img>
+				<img className="resizeReactions" src="../images/disinterested.png" alt="disinterested"></img>
+				<img className="resizeReactions" src="../images/excited.png" alt="excited"></img>
+				<img className="resizeReactions" src="../images/happy.png" alt="happy"></img>
+				<img className="resizeReactions" src="../images/sad.png" alt="sad"></img>
+				</div>
+			</div>
+				
+				
 			</div>
 			<div className="login-page">
 				<div className="login-container">
